8403bc39e61123de61da4b95f66ed5b7e205e235
Dereck Portela <dereck_portela@apple.com>
2021-06-25 12:23:06 -0300

    Add post users

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -3,5 +3,7 @@ public class Constants {
     public static final String MESSAGE_SUCCESS_DELETION = "Registro excluído com sucesso";
     public static final String MESSAGE_SUCCESS_LOGIN = "Login realizado com sucesso";
     public static final String MESSAGE_FAILED_LOGIN = "Email e/ou senha inválidos";
+    public static final String POST_USER_SUCCESS = "Cadastro realizado com sucesso";
+    public static final String POST_USER_FAIL = "Este email já está sendo usado";
 
 }
